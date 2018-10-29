0b323d450ee624b974604c47362c92c82edf2bb5
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-10-29 13:30:14 -0700

    learning fundamentals of imports and exports

---
+++ app/math.js
@@ -0,0 +1,11 @@
+export function add(x, y){
+    return x+y;
+}
+
+export const subtract = (x,y) => x-y;
+
+export const multiply = (z,m) => z*m;
+
+export const divide = (z,m) => m ? z/m : "can't divide by 0";
+
+export default 'This is just a string';
